c6940b7800830ed9bf62c0989b74c292d00c0004
Pavel Timofeev <ptimofeev@sphereconsultinginc.com>
2011-03-14 11:47:56 +0300

    Initial structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyists::Application.config.secret_token = '96f9516b7a18e23245076775ee1a89c2b8f8768a48ef002735b3753adbbc38014fe60019195ec91b04a864ac45297745a8e720d6ed72da48a9c59f68ec86e280'
